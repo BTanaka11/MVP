2143631c7ce90fd1c36ab5d5ce22029419e0be07
Ben Tanaka <BenT07891@gmail.com>
2022-11-14 10:04:04 -0600

    start of project, added folder structure

---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import App from './components/App.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />)
