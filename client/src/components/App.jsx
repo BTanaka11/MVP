8bde9db9ba2a11835d53078b1894307382129681
Ben Tanaka <BenT07891@gmail.com>
2022-11-14 10:26:27 -0600

    base structure up and running

---
+++ client/src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = ()=> {
+  return (
+    <div>i am render</div>
+  )
+}
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
